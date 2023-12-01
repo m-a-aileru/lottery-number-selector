26966c8f20eed8b22cbdf6d9af77c993d7166546
Matthew Aileru <m.a.aileru@gmail.com>
2023-12-01 04:56:50 +0000

    Updated the layout
    Updated the layout and interface slightly

--- app.py
+++ app.py
@@ -1,42 +1,61 @@
 from tkinter import *
 import random
 
+# num = StringVar()
 
-def output():
+
+def generate_numbers():
     lot_nums = set()
 
     while len(lot_nums) < 6:
         lot_nums.add(random.randrange(1, 60))
 
-    print(lot_nums)
+    numbers_string.set(str(lot_nums))
+    button_text.set("Generate More Numbers")
 
 
-def launch_app():
-    root = Tk()
-    root.geometry("640x480")
-    frame = Frame(root, bd=5, bg="purple")
-    frame.pack()
+# def launch_app():
+root = Tk()
 
-    midframe = Frame(root, bg="blue", bd=3)
-    midframe.pack(side=BOTTOM)
+root.geometry("420x140")
+frame = Frame(root, bd=5, bg="white")
+frame.pack()
 
-    label = Label(frame, text="Lottery Number Selector")
-    label.pack()
+numbers_string = StringVar()
+numbers_string.set(">> Your numbers will appear here <<")
 
-    button = Button(
-        midframe,
-        text="Generate Numbers",
-        command=output,
-        fg="green",
-        font="Verdana 12 bold",
-        bd=2,
-        bg="light blue",
-        relief="groove",
-    )
-    button.pack(padx=5, pady=5)
+button_text = StringVar()
+button_text.set("Generate Numbers")
 
-    root.title("Lottery Number Selector")
-    root.mainloop()
+# app_name_label = Label(frame, text="Lottery Number Selector")
+# app_name_label.pack()
+
+numbers_label = Label(
+    frame,
+    textvariable=numbers_string,
+    bg="yellow",
+    fg="green",
+    height=3,
+    width=100,
+    font="Tahoma 16 bold",
+)
+numbers_label.pack()
+
+button = Button(
+    frame,
+    textvariable=button_text,
+    command=generate_numbers,
+    font="Tahoma 14 bold",
+    bd=2,
+    bg="green",
+    fg="gold",
+    relief="groove",
+    width=80,
+)
+button.pack(padx=5, pady=5)
+
+root.title("Lottery Number Selector")
+root.mainloop()
 
 
-launch_app()
+# launch_app()
